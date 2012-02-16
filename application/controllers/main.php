3178751fedc6e86c296aa0d7f25cd8a4ace51f16
Therin Irwin <tcirwin@calpoly.edu>
2012-02-15 23:21:44 -0800

    Removed welcome message file. Started working on the Experiment Hierarchy page.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -29,6 +29,7 @@ class Main extends CI_Controller {
       $this->template->write_view('content', 'home.php');
       $this->template->write_view('content', 'testTable.php');
       $this->template->write_view('content', 'upload.php');
+      $this->template->write_view('content', 'expHierarchy.php');
       $this->template->add_js('js/mainPage.js');
       $this->_renderPage();
 	}
---
+++ application/views/expHierarchy.php
@@ -0,0 +1,58 @@
+<div class="tab-pane" id="hierarchyPane">
+<div class="row">
+   <div class="span3"> 
+      <h3>Species</h3>
+      <div class="drilldown"> 
+      <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="testTable">
+         <tbody>
+            <tr><td>lol this is a table</td></tr>
+            <tr><td>lol this is a table</td></tr>
+            <tr><td>lol this is a table</td></tr>
+            <tr><td>lol this is a table</td></tr>
+            <tr><td>lol this is a table</td></tr>
+            <tr><td>lol this is a table</td></tr>
+            <tr><td>lol this is a table</td></tr>
+            <tr><td>lol this is a table</td></tr>
+            <tr><td>lol this is a table</td></tr>
+            <tr><td>lol this is a table</td></tr>
+         </tbody>
+      </table>
+      </div>
+   </div>
+   <div class="span3 selector">
+ 
+   </div>
+   <div class="span3 selector">
+   
+   </div>
+   <div class="span3 selector">
+      
+   </div>
+</div>
+<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="testTable">
+   <thead>
+      <tr>
+         <th>Name</th>
+         <th>Grade</th>
+         <th>Number</th>
+      </tr>
+   </thead>
+   <tbody>
+      <tr>
+         <td>Al</td>
+         <td>B</td>
+         <td>3</td>
+      </tr>
+      <tr>
+         <td>Bob</td>
+         <td>C</td>
+         <td>1</td>
+      </tr>
+      <tr>
+         <td>Carl</td>
+         <td>A</td>
+         <td>2</td>
+      </tr>
+   </tbody>
+</table>
+</div>
