e910cef8203408892173ac2c528223d77dd0f7bd
Sterling Hirsh <sterling@sterlinghirsh.com>
2012-03-04 13:40:54 -0800

    Fix ccidentally committed change to config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://brovine.csc.calpoly.edu/~tcirwin/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
