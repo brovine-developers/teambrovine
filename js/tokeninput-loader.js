fc991c402504d2a53314633317e4170422908c71
Therin Irwin <therin2006@gmail.com>
2012-07-12 20:28:48 -0700

    Added jquery TokenInput to project to support adding/removing regulation filters. Wrote some basic code to initialize and change the data in the gene table when a new filter is selected. WIP

---
+++ js/tokeninput-loader.js
@@ -0,0 +1,33 @@
+/**
+ * Creates a tokenInput box out of a regular textbox. "elem" should be
+ * a jquery selector pointing at one text box.
+ */
+function loadTokenInput(elem) {
+   fireToken = function (item) { return updateRegFilter(elem); };
+
+   $(elem).tokenInput("ajax/searchReg", onAdd: fireToken,
+    onDelete: fireToken);
+}
+
+/**
+ * Send an ajax call updating the Genes table each time regulation filter
+ * item is added or deleted.
+ */
+function updateRegFilter(elem) {
+   var tokens = $(elem).tokenInput("get");
+   var names = [];
+
+   $.each(tokens, function (idx, item) {
+      names.push(item.name);
+   });
+}
+
+/*Riverwood
+21604 Moscow Rd.
+Monte Rio
+
+beach towels
+blue mat
+garbage cans in
+mail to guerneville
+foldy chair*/
