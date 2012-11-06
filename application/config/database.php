1efa36225535efeb504e18b6e72a467ac2f6cdfa
Therin Irwin <therin.irwin@gmail.com>
2012-11-06 14:00:42 -0800

    Removed MySQL user+pass from repo

--- application/config/database.php
+++ application/config/database.php
@@ -45,25 +45,8 @@
 | the active record class
 */
 
-$active_group = 'default';
-$active_record = TRUE;
-
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'new-password';
-$db['default']['database'] = 'brovine';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
-
+// All user + pass info moved to passwd.php, which is not stored in the repo.
+require('passwd.php');
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
