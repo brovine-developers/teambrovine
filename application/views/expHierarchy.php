54da74554e743057dcf1509f538bc5d8e6d0672e
Therin Irwin <therin2006@gmail.com>
2013-05-03 15:00:31 -0700

    Try a new regulation filter layout

--- application/views/expHierarchy.php
+++ application/views/expHierarchy.php
@@ -95,68 +95,58 @@
       <a class="btn" id="factorExport"><i class="icon-download"></i>Export Data</a>
    </div>
 </div>
+
 <div class="row">
    <div class="span12">
       <div class="well">
-         <h3>Regulatory Sequence Filter Options</h3>
-<? /* TODO: Add input type="number" if you feel like setting up
-the event listeners for it. Need to get click, keypress, 
-and scroll. */ ?>
-         <div class="row" id="sequenceFilterOptions">
-            <div class="span1">
+         <h3 class="filter-label">Regulatory Sequence Filter: </h3>
+
+         <div id="sequenceFilterOptions">
+            <div class="control-group">
                <label>Min La<br />
                   <input type="text" class="span1" id="minla">
                </label>
-            </div>
-            <div class="span1">
+
                <label>Min La/<br>
                   <input type="text" class="span1" id="minlaslash">
                </label>
-            </div>
-            <div class="span1">
+
                <label>Min Lq<br>
                   <input type="text" class="span1" id="minlq">
                </label>
-            </div>
-            <div class="span1">
+
                <label>Max Ld<br>
                   <input type="text" class="span1" id="maxld">
                </label>
             </div>
-            <div class="span1 offset1">
+
+            <div class="control-group">
                <label>Min Beg<br>
                   <input type="text" class="span1" id="minbeg">
                </label>
-            </div>
-            <div class="span1">
+
                <label>Max Beg<br>
                   <input type="text" class="span1" id="maxbeg">
                </label>
             </div>
-            <div class="span3 offset1 form-inline">
-               <div class="row">
-                  <div class="span1">Sense</div>
-               </div>
-               <div class="row" id="senseFilters">
-                  <div class="span1">
-                  <label class="radio">
-                     <input type="radio" name="senseFilter" value="all" checked>
-                     All
-                  </label>
-                  </div>
-                  <div class="span1">
-                  <label class="radio">
-                     <input type="radio" name="senseFilter" value="N">
-                     N
-                  </label>
-                  </div>
-                  <div class="span1">
-                  <label class="radio">
-                     <input type="radio" name="senseFilter" value="R">
-                     R
-                  </label>
-                  </div>
-               </div>
+
+            <div class="control-group">
+               <span class="sense-label">Sense</span>
+
+               <label class="radio">
+                  <input type="radio" name="senseFilter" value="all" checked>
+                  All
+               </label>
+
+               <label class="radio">
+                  <input type="radio" name="senseFilter" value="N">
+                  N
+               </label>
+
+               <label class="radio">
+                  <input type="radio" name="senseFilter" value="R">
+                  R
+               </label>
             </div>
          </div>
       </div>
