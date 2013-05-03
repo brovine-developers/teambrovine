19018837a01b59d8487cf59b5c17028ec631a779
Therin Irwin <therin2006@gmail.com>
2013-05-03 14:54:09 -0700

    Move input inside of the label

--- application/views/expHierarchy.php
+++ application/views/expHierarchy.php
@@ -104,8 +104,9 @@ the event listeners for it. Need to get click, keypress,
 and scroll. */ ?>
          <div class="row" id="sequenceFilterOptions">
             <div class="span1">
-               <label>Min La</label>
+               <label>Min La<br />
                   <input type="text" class="span1" id="minla">
+               </label>
             </div>
             <div class="span1">
                <label>Min La/<br>
